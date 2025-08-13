ee2289138443ef389206b843abbb5793ec2f0afa
ryanwilliamdev <rywilldav@gmail.com>
2025-08-13 10:34:56 -0400

    update main.js and index.html to display input zip code temperature and conditions to the DOM

--- weather-checker/js/main.js
+++ weather-checker/js/main.js
@@ -1,21 +1,20 @@
-//Example fetch using pokemonapi.co
+//Fetch using weatherapi.com
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://api.nasa.gov/planetary/apod?api_key=7ySrOTRJXadmUQusiAa9DMCW4oApKmsfq1QPQWXT&date=${choice}`
+  const zipCode = document.querySelector('input').value
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=0eeb750b8be0481f90b134959251308&q=${zipCode}&days=1&aqi=no&alerts=no`
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if (data.media_type === 'image') {
-          document.querySelector('img').src = data.hdurl
-        } else if (data.media_type === 'video') {
-          document.querySelector('iframe').src = data.url
-        }
-        document.querySelector('h3').innerText = data.explanation
-      })
+        const currentTempInFahr = data.current.temp_f
+        const currentConditionsText = data.current.condition.text.toLowerCase()
+        document.querySelector('#location').innerText = `${data.location.name}, ${data.location.region}`
+        document.querySelector('#condition').innerText = `The current temperature is ${currentTempInFahr}\u00B0F . The current conditions are ${currentConditionsText}.`
+        document.querySelector('img').src = data.current.condition.icon
+        })
       .catch(err => {
           console.log(`error ${err}`)
       });
